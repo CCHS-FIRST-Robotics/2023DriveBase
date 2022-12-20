239b19de332ba0895abded4a5cf18e8a7eab675c
pereos <danieljiang2358@gmail.com>
2022-12-20 15:50:06 -0500

    created network class

--- src/main/java/frc/robot/Network.java
+++ src/main/java/frc/robot/Network.java
@@ -1,79 +1,53 @@
-package frc.robot.network;
+package frc.robot;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import frc.robot.state.MainState;
 
 import java.util.Random;
 
 public class Network {
-    NetworkTableInstance inst;
+    NetworkTableInstance instance;
 
-    NetworkTableEntry start_time;
-    NetworkTableEntry emit_time;
+    // Positional
+    NetworkTableEntry posX;
+    NetworkTableEntry posY;
+    NetworkTableEntry heading;
 
-    NetworkTableEntry x_pos;
-    NetworkTableEntry y_pos;
+    // DEBUG
+    NetworkTableEntry currentFPS;
+    NetworkTableEntry droppedFrames;
 
-    public StereoNet stereo_net;
-    public Lidar lidar;
-    public BallNet ball_net;
-    public LimeNet lime;
 
     public Network() {
-        this.
+        this.instance = NetworkTableInstance.getDefault();
+    }
+    
+
+    public void receiveTable() {
+        NetworkTable state = instance.getTable("state");
+
+        posX = state.getEntry("x_pos");
+        posX.setDouble(0);
+
+        posY = state.getEntry("y_pos");
+        posY.setDouble(0);
+
+        heading = state.getEntry("heading");
+        heading.setDouble(0);
     }
 
-    public void init(double init_time) {
-        inst = NetworkTableInstance.getDefault();
-        NetworkTable sync = inst.getTable("Sync");
+    public void receiveDebug() {
+        NetworkTable debug = instance.getTable("debug");
 
-        this.start_time = sync.getEntry("start_time");
-        this.start_time.setDouble(init_time);
+        currentFPS = debug.getEntry("fps");
+        currentFPS.setDouble(0);
 
-        this.emit_time = sync.getEntry("emit_time");
-        this.emit_time.setDouble((double) System.currentTimeMillis() / 1000);
-
-        NetworkTable tab = inst.getTable("State");
-
-        this.x_pos = tab.getEntry("x_pos");
-        this.x_pos.setDouble(0);
-
-        this.y_pos = tab.getEntry("y_pos");
-        this.y_pos.setDouble(0);
-
-        this.heading = tab.getEntry("heading");
-        this.heading.setDouble(0);
-
-        this.x_vel = tab.getEntry("x_vel");
-        this.x_vel.setDouble(0);
-
-        this.y_vel = tab.getEntry("y_vel");
-        this.y_vel.setDouble(0);
-
-        this.a_vel = tab.getEntry("a_vel");
-        this.a_vel.setDouble(0);
-
-        this.stereo_net.init(inst);
-        this.lidar.init(inst);
-        this.ball_net.init(inst);
-        this.lime.init(inst);
+        droppedFrames = debug.getEntry("dropped");
+        droppedFrames.setDouble(0);
     }
 
-    public void writeNTable(MainState state) {
-        this.emit_time.setDouble((double) System.currentTimeMillis() / 1000);
+    public void sendTable() {
 
-        double[] pos = state.getPosVal();
-        this.x_pos.setDouble(pos[0]);
-        this.y_pos.setDouble(pos[1]);
-
-        this.heading.setDouble(state.getHeadingVal());
-
-        this.x_vel.setDouble(state.getVelVal()[0]);
-        this.y_vel.setDouble(state.getVelVal()[1]);
-
-        this.a_vel.setDouble(state.getAngVelVal());
-        this.inst.flush();
     }
 }
