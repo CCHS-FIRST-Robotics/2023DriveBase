5812ae13d185fdc2a760aa817d7818b0ed0d41c6
xoth42 <xoth42@proton.me>
2023-01-10 12:24:09 -0500

    doxygen bare

--- docs/html/search/files_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['subclass_2dexample_2ehpp_0',['subclass-example.hpp',['../subclass-example_8hpp.html',1,'']]]
-];
--- docs/html/search/pages_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['extensions_0',['Extensions',['../md_docs_extensions.html',1,'']]]
-];
