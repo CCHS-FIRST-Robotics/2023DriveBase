2541f83e87712fcc046f4426bf51992d7381a7b5
xoth42 <98821058+xoth42@users.noreply.github.com>
2023-01-24 16:43:29 -0500

    fix

--- docs/html/_constants_8java.js
+++
@@ -1,4 +0,0 @@
-var _constants_8java =
-[
-    [ "frc.robot.Constants", "classfrc_1_1robot_1_1_constants.html", "classfrc_1_1robot_1_1_constants" ]
-];
--- docs/html/_meca_drive_8java.js
+++
@@ -1,4 +0,0 @@
-var _meca_drive_8java =
-[
-    [ "frc.robot.subsystems.MecaDrive", "classfrc_1_1robot_1_1subsystems_1_1_meca_drive.html", "classfrc_1_1robot_1_1subsystems_1_1_meca_drive" ]
-];
