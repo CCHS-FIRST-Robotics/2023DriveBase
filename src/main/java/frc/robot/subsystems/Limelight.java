5605c0e94fecb88d2f4932eee5867f27d1c5a731
Colin Balfour <75389927+ColinBalfour@users.noreply.github.com>
2023-02-03 16:00:08 -0500

    updated limelight code

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -10,9 +10,8 @@ public class Limelight {
     double x, y, area;
     NetworkTableEntry tx, ty, ta;
     NetworkTable table;
-    double test2 = 0;
 
-    public void limelight() {
+    public Limelight() {
 
         table = NetworkTableInstance.getDefault().getTable("limelight");
 
@@ -20,26 +19,17 @@ public class Limelight {
         ty = table.getEntry("ty");
         ta = table.getEntry("ta");
 
-        x    = tx.getDouble(0);
-        y    = ty.getDouble(0);
-        area = ta.getDouble(0);
-
-    }
-
-    public void printVal() {
-        System.out.println("limeval " + x);
     }
 
     public void smartDash() {
+
+        x    = tx.getDouble(0);
+        y    = ty.getDouble(0);
+        area = ta.getDouble(0);
 
         SmartDashboard.putNumber("LimelightX", x);
         SmartDashboard.putNumber("LimelightY", y);
         SmartDashboard.putNumber("LimelightArea", area);
 
-    }
-
-    public void test(){
-        SmartDashboard.putNumber("test2", test2);
-        test2 += 1;
     }
 }
