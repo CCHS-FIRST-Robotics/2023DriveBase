35eb8088ec48a4230b2819f385aeab6020f72d65
Colin Balfour <75389927+ColinBalfour@users.noreply.github.com>
2023-02-04 18:48:06 -0500

    Create Controller.java

---
+++ src/main/java/frc/robot/subsystems/Controller.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.Constants;
+
+public class Controller extends XboxController{
+
+    public Controller() {
+        super(Constants.XBOX_CONTROLLER_PORT);
+    }
+}
