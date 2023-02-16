38247eced0a4e548be9519c1b0143531a984d07a
Avani Rebbi <azrebbi@gmail.com>
2023-02-15 22:08:56 -0500

    pnuematics code and comments f/lime & pnuematics

---
+++ src/main/java/frc/robot/subsystems/Grabber.java
@@ -0,0 +1,76 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
+
+public class Grabber {
+DoubleSolenoid solenoidPCMClaw, solenoidPCMWrist;
+Compressor pcmCompressor;
+
+    /**
+    * Constructor for Grabber Class
+    * 
+    * @param clawForwardChannelNum
+    * @param clawReverseChannelNum
+    * @param wristForwardChannelNum
+    * @param wristReverseChannelNum
+    */
+    public Grabber(int clawForwardChannelNum, int clawReverseChannelNum, 
+                    int wristForwardChannelNum, int wristReverseChannelNum) {
+        // Each solenoid has two sets of positive and negative leads, one responsible for going forward the other for going back
+        // Therefore, each object is passed the appropriate numbers that correlate with the values
+        solenoidPCMClaw  = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, clawForwardChannelNum,  clawReverseChannelNum);
+        solenoidPCMWrist = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, wristForwardChannelNum, wristReverseChannelNum);
+
+        pcmCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
+        
+        // This if statment to make sure the compressor runs, if it is not running
+        if(!compIsEnabled())
+            pcmCompressor.isEnabled();
+        else
+            pcmCompressor.enableDigital();
+            //used in this case because a switch (modulated by the CTRE Pneumatics Control Module) regulates the compressor
+    }
+
+    // For the next six functions set() is used to accordingly make the pnuematic go forward, go backward, or stop
+    public void wristForward(){
+        solenoidPCMWrist.set(Value.kForward);
+    }
+
+    public void wristBack(){
+        solenoidPCMWrist.set(Value.kReverse);
+    }
+
+    public void wristStop(){
+        solenoidPCMWrist.set(Value.kOff);
+    }
+
+    public void clawForward(){
+        solenoidPCMClaw.set(Value.kForward);
+    }
+
+    public void clawBack(){
+        solenoidPCMClaw.set(Value.kReverse);
+    }
+
+    public void clawStop(){
+        solenoidPCMClaw.set(Value.kOff);
+    }
+
+    // just for you colin, this function (in case you didn't guess it) passes back whether the compressor is enabled
+    // this is for conveincice, so we can call the function from anywhere in the program witout needing the compressor object
+    public boolean compIsEnabled() {
+        return pcmCompressor.isEnabled();
+    }
+
+
+
+}
+
+
+// Other variables we can set up if we want (they were set up in example code):
+//  boolean enabled = pcmCompressor.enabled();
+//  boolean pressureSwitch = pcmCompressor.getPressureSwitchValue();
+//  double current = pcmCompressor.getCompressorCurrent();
--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,67 +1,84 @@
 package frc.robot.subsystems;
-
 import frc.robot.Constants;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import java.lang.Math;
-
-import javax.lang.model.util.ElementScanner14;
 
 public class Limelight {
-
+    // What each of the variables from the pipline represent:
+    //   tv --> Whether the limelight has any valid targets (0 or 1)
+    //   tx --> Horizontal Offset From Crosshair To Target (-27 degrees to 27 degrees)
+    //   ty --> Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees)
+    //   ta --> Target Area (0% of image to 100% of image)
+    // For more information refer to: https://docs.limelightvision.io/en/latest/getting_started.html 
     private double x, y, area, forwardDistance;
     private int pipeNum;
 
+    // This makes the network table so that the liemlight can pass down new values via the pipeline
+    // There are multiple pipelines so that different sets of values can be passed down
+    // Each pipline is tuned for its respective target, and its values give information on that target
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
 
-
+    // This calculates the forward distance using trig
+    // The forward distance is the equivalent of adjacent side in the triangle
+    // The highet from the lens to the target is the opposite side of the triangle
+    // With the measured verticle angle, we can use tangent to calculate the adjacent side (aka the forward distance)
+    // For more help go to: https://www.dummies.com/article/academics-the-arts/math/trigonometry/trigonometry-for-dummies-cheat-sheet-207754/ 
     public double getForwardDistance(int pipeChoice) {
         changePipeline(pipeChoice);
 
         double goalHeight = 0;
 
+        // This toggles between target heights (needed for calculating the opposite side)
         if (pipeChoice == 0) {
             goalHeight = Constants.SHORT_TARGET_HEIGHT;
-        } else if (pipeChoice == 1) {
+        } 
+        else if (pipeChoice == 1) {
             goalHeight = Constants.TALL_TARGET_HEIGHT;
         }
 
         double limelightLensHeight        = Constants.LIME_HEIGHT;
-        double limelightLensHeight        = Constants.LIME_HEIGHT;
         double limelightMountAngle        = Constants.LIME_ANGLE;
-        double targetOffsetAngleVertical = table.getEntry("ty").getDouble(0.0) * ((Math.PI)/(180));
+        double targetOffsetAngleVertical  = table.getEntry("ty").getDouble(0.0) * ((Math.PI)/(180));
+        // Last part of that line converts the angle to radians
 
         double angleToGoal = limelightMountAngle + targetOffsetAngleVertical;
 
-        forwardDistance = (goalHeightInches - limelightLensHeight)/Math.tan(angleToGoal);
+        forwardDistance = (goalHeight - limelightLensHeight)/Math.tan(angleToGoal);
 
         return forwardDistance;
     }
     
+    // Not sure why this is here --> we need to take this out later
     public double getHeadingDisplacement(int pipeChoice) {
         changePipeline(pipeChoice);
         return table.getEntry("tx").getDouble(0.0) * ((Math.PI)/(180));
     }
 
-    // TODO: include comments/documentation for what these vals actually are 
+    // This passes back how many degrees it is off by horixontally
     public double getX(int pipeChoice) {
         changePipeline(pipeChoice);
         x = table.getEntry("tx").getDouble(0);
         return x;
     }
     
+    // This passes back how many degrees it is off by vertically
     public double getY(int pipeChoice) {
         changePipeline(pipeChoice);
         y = table.getEntry("ty").getDouble(0);
         return y;
    }
     
+   // This passes back how much of the camera lens the target fills
     public double getArea(int pipeChoice) {
         changePipeline(pipeChoice);
         area = table.getEntry("ta").getDouble(0);
         return area;
     }
 
+    // This should (in theory) switch the pipeline so that we can look at one target then another
+    // In the 2023 game this means that we can switch between the high and low reflective tape on the poles
+    // Pipeline 0 is for the low target
+    // Pipeline 1 is for the high target
     public void changePipeline(int newPipeNum){
         if (!table.getEntry("pipeline").setInteger(newPipeNum))
         {
@@ -81,6 +98,8 @@ public class Limelight {
         pipeNum = newPipeNum;
     }
 
+    // Returns the last number the pipeline was supposed to be
+    // Currently, this may not be accurate because the pipeline seems to have issues switching
     public int getPipeline(){
         return pipeNum;
     }
