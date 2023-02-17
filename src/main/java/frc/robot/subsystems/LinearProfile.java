f6433f8f35f903358c62620650f6a5ec84048496
ColinBalfour <75389927+ColinBalfour@users.noreply.github.com>
2023-02-16 23:41:49 -0500

    ting

--- src/main/java/frc/robot/subsystems/LinearProfile.java
+++ src/main/java/frc/robot/subsystems/LinearProfile.java
@@ -12,23 +12,34 @@ public class LinearProfile {
 
     
     public static double[] getSetPoints(double[] initialPosition, double[] finalPosition, double[] initialVelocity) {
+        double timeToEnd = magnitude(subtract(finalPosition, initialPosition)) / Constants.ARM_MAX_VELOCITY;
+
+        int numberOfSteps = (int) (Math.ceil(timeToEnd / Constants.PERIOD));
+        double[] setpoints = new double[numberOfSteps];
+
+        for (int i = 0; i < numberOfSteps; i++) {
+            setpoints[i] = i * Constants.PERIOD;
+        }
         
-        
-        return new double[1];
+        return setpoints;
     }
 
-    private static double[] getAcceleratingSetPoints(double timeInterval) {
+    // private static double[] getAcceleratingSetPoints(double timeInterval) {
 
-        int numberOfSteps = (int) (Math.ceil(timeInterval / Constants.PERIOD));
+    //     int numberOfSteps = (int) (Math.ceil(timeInterval / Constants.PERIOD));
 
-        double[] setPoints = new double[numberOfSteps];
+    //     double[] setPoints = new double[numberOfSteps];
 
-        // loops through all the time increments
-        for(int i = 0; i < numberOfSteps; i++) {
-            double positionMag = Constants.ARM_MAX_ACCELERATION / 2 * Constants.PERIOD * Constants.PERIOD * (i+1) * (i+1);
-        }
+    //     // loops through all the time increments
+    //     for(int i = 0; i < numberOfSteps; i++) {
+    //         double positionMag = Constants.ARM_MAX_ACCELERATION / 2 * Constants.PERIOD * Constants.PERIOD * (i+1) * (i+1);
+    //     }
 
-        return new double[1];
+    //     return new double[1];
+    // }
+
+    private static double[] subtract(double[] array1, double[] array2) {
+        return new double[]{array1[0] - array2[0], array1[1] - array2[1]};
     }
 
     private static double magnitude(double[] array) {
