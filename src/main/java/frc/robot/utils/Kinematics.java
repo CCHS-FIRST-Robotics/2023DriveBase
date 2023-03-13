dd544613af391d7d8b210af58cc3983540149143
ColinBalfour <75389927+ColinBalfour@users.noreply.github.com>
2023-03-13 13:52:59 -0400

    should work now, did some tests. motor lims back

--- src/main/java/frc/robot/utils/Kinematics.java
+++ src/main/java/frc/robot/utils/Kinematics.java
@@ -301,45 +301,43 @@ public final class Kinematics {
 	}
 
 	public static boolean isMovingPastLimit(double alpha, double beta, double theta, double directionX, double directionY) {
+		double[] posEE = Kinematics.forwardKinematics(alpha, beta, theta);
+		double x = posEE[0];
+		double y = posEE[1];
+
+		double[] posWrist = Kinematics.forwardKinematics(alpha, beta);
+		double wristX = posWrist[0];
+		double wristY = posWrist[1];
+
+		if (
+			!((x > Constants.minX || directionX > 0) &&
+			(x < Constants.maxX || directionX < 0) &&
+			isPositionPossible(wristX, wristY))
+		) return true;
+
+		if (
+			!((y > Constants.minY || directionY > 0) &&
+			(y < Constants.maxY || directionY < 0) &&
+			Kinematics.isPositionPossible(wristX, wristY))
+		) return true;
+
+		if (
+			// check if the shoulder is too far forward/backward
+			alpha < Constants.minAlpha ||
+			alpha > Constants.maxAlpha ||
+
+			// check if the elbow is too far forward/backward
+			beta < Constants.minBeta ||
+			beta > Constants.maxBeta
+		) return true;
+
+		// // check to make sure the arm isn't hitting the frame
+		if (
+			(Constants.isInFrameX(x) && Constants.isBelowFrame(y)) ||
+			(directionX > 0 && directionY > 0)
+		) return true;
+
 		return false;
-		// return shouldMotorStop(alpha, beta, theta);
-		// double[] posEE = Kinematics.forwardKinematics(alpha, beta, theta);
-		// double x = posEE[0];
-		// double y = posEE[1];
-
-		// double[] posWrist = Kinematics.forwardKinematics(alpha, beta);
-		// double wristX = posWrist[0];
-		// double wristY = posWrist[1];
-
-		// if (
-		// 	!((x > Constants.minX || directionX > 0) &&
-		// 	(x < Constants.maxX || directionX < 0) &&
-		// 	isPositionPossible(wristX, wristY))
-		// ) return true;
-
-		// if (
-		// 	!((y > Constants.minY || directionY > 0) &&
-		// 	(y < Constants.maxY || directionY < 0) &&
-		// 	Kinematics.isPositionPossible(wristX, wristY))
-		// ) return true;
-
-		// if (
-		// 	// check if the shoulder is too far forward/backward
-		// 	alpha < Constants.minAlpha ||
-		// 	alpha > Constants.maxAlpha ||
-
-		// 	// check if the elbow is too far forward/backward
-		// 	beta < Constants.minBeta ||
-		// 	beta > Constants.maxBeta
-		// ) return true;
-
-		// // // check to make sure the arm isn't hitting the frame
-		// if (
-		// 	(Constants.isInFrameX(x) && Constants.isBelowFrame(y)) ||
-		// 	(directionX > 0 && directionY > 0)
-		// ) return true;
-
-		// return false;
 	}
 
 	public static boolean isPositionPossible(double x, double y) {
--- src/main/java/frc/robot/utils/QuadraticProfile.java
+++ src/main/java/frc/robot/utils/QuadraticProfile.java
@@ -18,70 +18,9 @@ public class QuadraticProfile {
     public QuadraticProfile() {
         this(.02);
     }
-    
-//    public double[][] getSetPoints(R2Vector initialPosition, R2Vector initialVelocity, R2Vector goal, double[] initialAngles, double speed, double acceleration) {
-//        R2Vector[] accelSetpoints, stoppingSetpoints, constantSpeedSetpoints;
-//        
-//        // displacement from the initial (x, y) to goal 
-//        R2Vector displacement = goal.sub(initialPosition);
-//        double angleDisplacement = displacement.dir(); // radians
-//
-//        R2Vector maxVelocity = new R2Vector(speed * Math.cos(angleDisplacement), speed * Math.sin(angleDisplacement));
-//        R2Vector changeInVelocity = maxVelocity.sub(initialVelocity);
-//
-//        double timeToAccelerate = changeInVelocity.mag() / acceleration;
-//        double accelerationAngle = changeInVelocity.dir();
-//        double timeToStop = maxVelocity.mag() / acceleration;
 
-        // check if we'll go past the setpoint/can't achieve max velocity and slow down in time
-//        if (0.5 * acceleration * Math.pow(timeToAccelerate, 2) > 0.5 * displacement.mag()) {
-//            // calculate time to go halfway
-//            timeToAccelerate = Math.sqrt(displacement.mag() / acceleration);
-//
-//            accelSetpoints = getAcceleratingSetPoints(timeToAccelerate, angleDisplacement);
-//            stoppingSetpoints = getStoppingSetpoints(timeToAccelerate, angleDisplacement);
-//            
-//            // never reach max velocity so there should be no setpoints at a constant velocity
-//            R2Vector[] temp = {new R2Vector(0, 0)};
-//            constantSpeedSetpoints = temp;
-//        } else {
-//            accelSetpoints = getAcceleratingSetPoints(timeToAccelerate, angleDisplacement);
-//            stoppingSetpoints = getStoppingSetpoints(timeToAccelerate, angleDisplacement);
-//    
-//            R2Vector lastAccelPoint = accelSetpoints[accelSetpoints.length - 1];
-//            R2Vector constantSpeedDisplacement = displacement.sub(lastAccelPoint.multiply(2));
-//            double timeConstantSpeed = constantSpeedDisplacement.mag() / Constants.ARM_MAX_SPEED;
-//            constantSpeedSetpoints = getConstantSpeedSetpoints(timeConstantSpeed, angleDisplacement);
-//        }
-//
-//        // System.out.println("ASUHDUHSUH" + accelSetpoints[45].x);
-//        System.out.println("#Accel: " + accelSetpoints.length);
-//        System.out.println("#Speed: " + constantSpeedSetpoints.length);
-//        System.out.println("#Stop: " + stoppingSetpoints.length);
-//
-//        R2Vector[] combined = combineSetPoints(accelSetpoints, constantSpeedSetpoints, stoppingSetpoints, initialPosition);
-//        
-//        
-//        
-//        double[][] setpoints = new double[combined.length][2];
-//        double[] angles;
-//        for (int i = 0; i < combined.length; i++) {
-//            if (i == 0) {
-//                angles = Kinematics.positionInverseKinematics(combined[i].x, combined[i].y, initialAngles);
-//            } else {
-//                angles = Kinematics.positionInverseKinematics(combined[i].x, combined[i].y, setpoints[i-1]);
-//            }
-//            
-//            setpoints[i][0] = angles[0];
-//            setpoints[i][1] = angles[1];
-//        }
-//        
-//        return setpoints;
-//    }
-
-    public ArrayList<double[]> getSetPoints(R2Vector initialPosition, double[] initialAngles, R2Vector goal, double theta, double speed, double acceleration) {
+    public R2Vector[] getCombinedSetPoints(R2Vector initialPosition, double[] initialAngles, R2Vector goal, double theta, double speed, double acceleration) {
         R2Vector[] accelSetpoints, stoppingSetpoints, constantSpeedSetpoints;
-        double directionX, directionY;
         
         // displacement from the initial (x, y) to goal 
         R2Vector displacement = goal.sub(initialPosition);
@@ -115,78 +54,75 @@ public class QuadraticProfile {
         System.out.println("#Speed: " + constantSpeedSetpoints.length);
         System.out.println("#Stop: " + stoppingSetpoints.length);
 
-        R2Vector[] combined = combineSetPoints(accelSetpoints, constantSpeedSetpoints, stoppingSetpoints, initialPosition);
+        return combineSetPoints(accelSetpoints, constantSpeedSetpoints, stoppingSetpoints, initialPosition);
+    }
+
+    public ArrayList<double[]> getSetPoints(R2Vector initialPosition, double[] initialAngles, R2Vector goal, double theta, double speed, double acceleration) {
+        double directionX, directionY;
         
-        ArrayList<double[]> pointsInR2 = new ArrayList<double[]>(combined.length);
-        for(int i = 0; i < combined.length; i++) {
-        	double[] vector = new double[2];
-        	vector[0] = combined[i].x;
-        	vector[1] = combined[i].y;
-        	pointsInR2.add(vector);
+        R2Vector[] combined = getCombinedSetPoints(initialPosition, initialAngles, goal, theta, speed, acceleration);
+        
+        ArrayList<double[]> setpoints = new ArrayList<double[]>(combined.length);
+        double[] angles;
+        for (int i = 0; i < combined.length; i++) {
+            try {
+                double x = combined[i].x;
+                double y = combined[i].y;
+                System.out.println("(x, y) = " + x + ", " + y);
+                if (i == 0) {
+                    angles = Kinematics.positionInverseKinematics(x, y, true);
+                    // angles = Kinematics.positionInverseKinematics(x, y, initialAngles);
+                } else {
+                    angles = Kinematics.positionInverseKinematics(x, y, true);
+                    // angles = Kinematics.positionInverseKinematics(x, y, setpoints.get(i-1));
+                }
+
+                double wristPosition = Kinematics.wristDesiredPosition(x, y);
+                // if its in between the two thresholds, assume it's flush with upper arm
+                if (wristPosition == -1) {
+                    wristPosition = 0;
+                }
+                
+                if (i == 0) {
+                    directionX = 0;
+                    directionY = 0;
+                } else {
+                    double prevX = combined[i-1].x;
+                    double prevY = combined[i-1].y;
+
+                    directionX = x - prevX;
+                    directionY = y - prevY;
+                }
+                    
+
+                if (Double.isNaN(angles[0]) || Double.isNaN(angles[1])) {
+                    throw new ArithmeticException("angle is NaN");
+                }
+                if (Kinematics.isMovingPastLimit(Math.toDegrees(angles[0]), Math.toDegrees(angles[1]), wristPosition, directionX, directionY)) {
+                    throw new Exception("(x, y) = (" + x + ", " + y + "), (a, b) = (" + angles[0] + ", " + angles[1] + ") " + "goes past a motor limit");
+                }
+            } catch(ArithmeticException e) 
+            {
+                System.out.println("X: " + combined[i].x);
+                System.out.println("Y: " + combined[i].y);
+                System.out.println(e.getMessage());
+                if (i != 0) {
+                    setpoints.add(setpoints.get(i-1));
+                }
+                continue;
+            } catch(Exception e) {
+                // If the motor is past a limit, stop the sequence
+                System.out.println("X: " + combined[i].x);
+                System.out.println("Y: " + combined[i].y);
+                System.out.println(e.getMessage());
+                
+                break;
+            }
+            
+            setpoints.add(angles);
         }
-        return pointsInR2;
-//        
-//        ArrayList<double[]> setpoints = new ArrayList<double[]>(combined.length);
-//        double[] angles;
-//        for (int i = 0; i < combined.length; i++) {
-//            try {
-//                double x = combined[i].x;
-//                double y = combined[i].y;
-//                System.out.println("(x, y) = " + x + ", " + y);
-//                if (i == 0) {
-//                    angles = Kinematics.positionInverseKinematics(x, y, true);
-//                    // angles = Kinematics.positionInverseKinematics(x, y, initialAngles);
-//                } else {
-//                    angles = Kinematics.positionInverseKinematics(x, y, true);
-//                    // angles = Kinematics.positionInverseKinematics(x, y, setpoints.get(i-1));
-//                }
-//
-//                double wristPosition = Kinematics.wristDesiredPosition(x, y);
-//                // if its in between the two thresholds, assume it's flush with upper arm
-//                if (wristPosition == -1) {
-//                    wristPosition = 0;
-//                }
-//                
-//                if (i == 0) {
-//                    directionX = 0;
-//                    directionY = 0;
-//                } else {
-//                    double prevX = combined[i-1].x;
-//                    double prevY = combined[i-1].y;
-//
-//                    directionX = x - prevX;
-//                    directionY = y - prevY;
-//                }
-//                 
-//
-//                if (Double.isNaN(angles[0]) || Double.isNaN(angles[1])) {
-//                    throw new ArithmeticException("angle is NaN");
-//                }
-//                if (Kinematics.isMovingPastLimit(Math.toDegrees(angles[0]), Math.toDegrees(angles[1]), wristPosition, directionX, directionY)) {
-//                    throw new Exception("(x, y) = (" + x + ", " + y + "), (a, b) = (" + angles[0] + ", " + angles[1] + ") " + "goes past a motor limit");
-//                }
-//            } catch(ArithmeticException e) 
-//            {
-//                System.out.println("X: " + combined[i].x);
-//                System.out.println("Y: " + combined[i].y);
-//                System.out.println(e.getMessage());
-//                if (i != 0) {
-//                    setpoints.add(setpoints.get(i-1));
-//                }
-//                continue;
-//            } catch(Exception e) {
-//                // If the motor is past a limit, stop the sequence
-//                System.out.println("X: " + combined[i].x);
-//                System.out.println("Y: " + combined[i].y);
-//                System.out.println(e.getMessage());
-//                
-//                break;
-//            }
-//            
-//            setpoints.add(angles);
-//        }
-//        System.out.println("GOT HERE -1");
-//        return setpoints;
+        System.out.println("GOT HERE -1");
+        return setpoints;
     }
 
     public R2Vector[] getAcceleratingSetPoints(double timeToEnd, double angle, double acceleration) {
